e4a1e7c428d28d037570c02dfdbdbbbb8689c02a
Saeed-Alsaiyed <msalsaiyed0@gmail.com>
2022-10-20 11:37:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return "This one is on me!"
+  } else if (feet<2000 && feet>400){
+    return "That will be twenty bucks."
+  }else if (feet > 2000 && feet<2500){
+    return "I will gladly take your thirty bucks."
+  }else if(feet > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(iscity){
   // Write your code here!
+  if (iscity === "NYC"){
+    return "Ok, sounds good."
+  } else{
+    return "No go.";
+  }
+   
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(type){
   // Write your code here!
+  switch(type){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+
+  }
 }
